a39d45cc2980264b08f86a49236d3afb5af084a7
Arnaud RITTI <arnaud.ritti@adeliom.com>
2024-04-29 13:12:35 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,150 @@
 //@ts-nocheck
-import { on } from "@create-figma-plugin/utilities";
+import { on, showUI } from "@create-figma-plugin/utilities";
 import type { ResizeWindowHandler } from "@typing/ResizeWindowHandler";
 import { exportToJSON } from "@utils/figma";
 import StyleDictionaryCore from "@StyleDictionary";
 import webConfig from "@StyleDictionary/web";
+import { format } from "style-dictionary";
 
-function showSettingsUI(options, data) {
-  if (typeof __html__ === 'undefined') {
-      throw new Error('No UI defined');
+const figmaModes = figma.variables
+  .getLocalVariableCollections()
+  .reduce((accumulator, collection) => {
+    return [...accumulator, ...collection.modes]
+  }, [])
+
+const generate = (settings) => {
+  if (!settings?.mode || !settings?.collection) {
+    throw new Error("Mode and collection are required");
   }
-  const html = `<div id="create-figma-plugin"></div><script>document.body.classList.add('theme-${figma.editorType}');const __FIGMA_COMMAND__="src/main.ts--default";const __SHOW_UI_DATA__=${JSON.stringify(typeof data === 'undefined' ? {} : data)};${__html__}</script>`;
-  figma.showUI(html, {
-      ...options,
-      themeColors: typeof options.themeColors === 'undefined' ? true : options.themeColors
-  });
-}
 
+  const outputReferences = settings.useReference;
+  const tokensCollection = exportToJSON(settings.collection, settings.mode);
+
+  const StyleDictionary = StyleDictionaryCore.extend({
+    source: [tokensCollection],
+    platforms: {
+      scss: {
+        transformGroup: "figma/css",
+        buildPath: "build/scss/",
+        files: [
+          {
+            destination: "_variables.scss",
+            format: "scss/variables",
+            filter: "validToken",
+            options: {
+              showFileHeader: false,
+              outputReferences: outputReferences,
+              unit: 'SCALED',
+              basePxFontSize: '16',
+            },
+          },
+        ],
+      },
+      less: {
+        transformGroup: "figma/css",
+        buildPath: "build/less/",
+        files: [
+          {
+            destination: "_variables.less",
+            format: "less/variables",
+            filter: "validToken",
+            options: {
+              showFileHeader: false,
+              outputReferences: outputReferences,
+              unit: 'SCALED',
+              basePxFontSize: '16',
+            },
+          },
+        ],
+      },
+      css: {
+        transformGroup: "figma/css",
+        buildPath: "build/css/",
+        files: [
+          {
+            destination: "_variables.css",
+            format: "css/awc-format",
+            filter: "validToken",
+            options: {
+              showFileHeader: false,
+              outputReferences: outputReferences,
+              unit: 'SCALED',
+              basePxFontSize: '16',
+              colorScheme: figmaModes.find((mode) => mode.modeId === settings?.mode)?.name
+            },
+          },
+        ],
+      },
+      "json-flat": {
+        transformGroup: "js",
+        buildPath: "build/json/",
+        files: [
+          {
+            destination: "styles.flat.json",
+            format: "json/flat",
+            filter: "validToken",
+          },
+        ],
+      },
+      "json-nested": {
+        transformGroup: "js",
+        buildPath: "build/json/",
+        files: [
+          {
+            destination: "styles.nested.json",
+            format: "json/nested",
+            filter: "validToken",
+          },
+        ],
+      }
+    },
+    ...webConfig
+  });
+
+  try {
+    StyleDictionary.buildPlatform("css");
+    StyleDictionary.buildPlatform("json-flat");
+    StyleDictionary.buildPlatform("json-nested");
+    const outputs = StyleDictionary.getOutput();
+
+    return {
+      'css': {
+        title: "CSS variables",
+        language: "html",
+        code: outputs['build/css/_variables.css'] || ''
+      },
+      'json': {
+        title: "JSON object",
+        language: "javascript",
+        code: outputs['build/json/styles.nested.json'] || ''
+      },
+      'json-flat': {
+        title: "Flat JSON object",
+        language: "javascript",
+        code: outputs['build/json/styles.flat.json'] || ''
+      }
+    }
+  } catch (error) {
+    return {
+      error: error
+    };
+  }
+
+};
 
 export default function () {
   let settings = figma.root.getPluginData('settings') ? JSON.parse(figma.root.getPluginData('settings')) : null;
-  if(!settings) {
+
+  if (!settings) {
     settings = figma.root.setPluginData('settings', JSON.stringify({
       collection: null,
       mode: null,
-      useReference: true
+      useReference: true,
+      format: 'css'
     }))
   }
 
-  const figmaModes = figma.variables.getLocalVariableCollections().reduce((accumulator, collection) => { return [...accumulator, ...collection.modes] }, [])
+  /**/
 
   on<ResizeWindowHandler>(
     'RESIZE_WINDOW',
@@ -40,163 +157,56 @@ export default function () {
   on('SUBMIT', async function (data) {
     settings = data;
     figma.root.setPluginData('settings', JSON.stringify(settings))
-    if (figma.editorType === "dev") {
-      if(figma.mode !== 'codegen') {
-        figma.closePlugin();
+    figma.ui.postMessage({ type: 'OUTPUT', data: generate(settings) })
+  });
+
+  on('COPY_TO_CLIPBOARD', async function (data) {
+    figma.notify(`${data.title} was copied to clipboard`)
+  })
+
+  showUI({ height: 600, width: 800 }, {
+    settings: settings,
+    collections: figma.variables.getLocalVariableCollections()
+      .map((collection) => {
+        return {
+          id: collection.id,
+          name: collection.name,
+          modes: collection.modes
+        }
+      }),
+  })
+
+  /*
+    figma.codegen.on("preferenceschange", ({propertyName}: CodegenPreferencesEvent): void => {   
+      if(propertyName === "otherSettings"){        
+        showSettingsUI({ height: 240,  width: 240 }, {
+          settings: settings,
+          collections: figma.variables.getLocalVariableCollections()
+            .map((collection) => {
+              return {
+                id: collection.id,
+                name: collection.name,
+                modes: collection.modes
+              }
+            }),
+        })
         return;
       }
       figma.codegen.refresh();
-    }
-  });
-
-  if (figma.editorType === "dev" && figma.mode !== 'codegen') {
-    showSettingsUI({ height: 240,  width: 240 }, {
-      settings: settings,
-      collections: figma.variables.getLocalVariableCollections()
-        .map((collection) => {
-          return {
-            id: collection.id,
-            name: collection.name,
-            modes: collection.modes
-          }
-        }),
-    })
-  }
-
-
-  figma.codegen.on("preferenceschange", ({propertyName}: CodegenPreferencesEvent): void => {   
-    if(propertyName === "otherSettings"){        
-      showSettingsUI({ height: 240,  width: 240 }, {
-        settings: settings,
-        collections: figma.variables.getLocalVariableCollections()
-          .map((collection) => {
-            return {
-              id: collection.id,
-              name: collection.name,
-              modes: collection.modes
-            }
-          }),
-      })
-      return;
-    }
-    figma.codegen.refresh();
-  });
-
-  figma.codegen.on("generate", ({ _language, _node }: CodegenEvent): CodegenResult[] => {
-    if(!settings?.mode || !settings?.collection) {
-      return [
-        {
-          title: "ERROR",
-          language: "PLAINTEXT",
-          code: "Invalid settings. \nPlease select a compatible collection with the corresponding mode."
-        }
-      ] 
-    }
-
-    const outputReferences = settings.useReference;
-    const tokensCollection = exportToJSON(settings.collection, settings.mode);
-    const StyleDictionary = StyleDictionaryCore.extend({
-      source: [tokensCollection],
-      platforms: {
-        scss: {
-          transformGroup: "figma/css",
-          buildPath: "build/scss/",
-          files: [
-            {
-              destination: "_variables.scss",
-              format: "scss/variables",
-              filter: "validToken",
-              options: {
-                showFileHeader: false,
-                outputReferences: outputReferences,
-                unit: figma.codegen.preferences.unit,
-                basePxFontSize: figma.codegen.preferences.scaleFactor
-              },
-            },
-          ],
-        },
-        less: {
-          transformGroup: "figma/css",
-          buildPath: "build/less/",
-          files: [
-            {
-              destination: "_variables.less",
-              format: "less/variables",
-              filter: "validToken",
-              options: {
-                showFileHeader: false,
-                outputReferences: outputReferences,
-                unit: figma.codegen.preferences.unit,
-                basePxFontSize: figma.codegen.preferences.scaleFactor
-              },
-            },
-          ],
-        },
-        css: {
-          transformGroup: "figma/css",
-          buildPath: "build/css/",
-          files: [
-            {
-              destination: "_variables.css",
-              format: "css/awc-format",
-              filter: "validToken",
-              options: {
-                showFileHeader: false,
-                outputReferences: outputReferences,
-                unit: figma.codegen.preferences.unit,
-                basePxFontSize: figma.codegen.preferences.scaleFactor,
-                colorScheme: figmaModes.find((mode) => mode.modeId === settings?.mode)?.name
-              },
-            },
-          ],
-        },
-        "json-flat": {
-          transformGroup: "js",
-          buildPath: "build/json/",
-          files: [
-            {
-              destination: "styles.flat.json",
-              format: "json/flat",
-              filter: "validToken",
-            },
-          ],
-        },
-        "json-nested": {
-          transformGroup: "js",
-          buildPath: "build/json/",
-          files: [
-            {
-              destination: "styles.nested.json",
-              format: "json/nested",
-              filter: "validToken",
-            },
-          ],
-        }
-      },
-      ...webConfig
     });
-    StyleDictionary.buildPlatform("css");
-    StyleDictionary.buildPlatform("json-flat");
-    StyleDictionary.buildPlatform("json-nested");
-    const outputs = StyleDictionary.getOutput();
-
-    return [
-      {
-        title: "CSS variables",
-        language: "CSS",
-        code: outputs['build/css/_variables.css'] || ''
-      },
-      {
-        title: "JSON object",
-        language: "JSON",
-        code: outputs['build/json/styles.nested.json'] || ''
-      },
-      {
-        title: "Flat JSON object",
-        language: "JSON",
-        code: outputs['build/json/styles.flat.json'] || ''
+  
+    figma.codegen.on("generate", ({ _language, _node }: CodegenEvent): CodegenResult[] => {
+      if(!settings?.mode || !settings?.collection) {
+        return [
+          {
+            title: "ERROR",
+            language: "PLAINTEXT",
+            code: "Invalid settings. \nPlease select a compatible collection with the corresponding mode."
+          }
+        ] 
       }
-    ]
-  })
-
+  
+      
+    })
+  */
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,48 +1,77 @@
-import { render, useWindowResize, Container, Text, VerticalSpace, DropdownOption, Dropdown, Button, useForm, Toggle, IconWarning32, Banner } from '@create-figma-plugin/ui'
+import { render, useWindowResize, Container, Text, VerticalSpace, DropdownOption, Dropdown, Button, useForm, Toggle, IconWarning32, Banner, Modal, IconCross32 } from '@create-figma-plugin/ui'
 import { emit } from "@create-figma-plugin/utilities";
 import { Fragment, h } from 'preact'
 import "!./assets/output.css";
 import type { ResizeWindowHandler } from "@typing/ResizeWindowHandler";
 import { useState, useEffect } from 'preact/hooks';
+import SyntaxHighlighter from "react-syntax-highlighter";
+import * as themes from "react-syntax-highlighter/dist/esm/styles/hljs";
+import supportedLanguages from 'react-syntax-highlighter/dist/cjs/languages/hljs/supported-languages';
+import { copyToClipboard } from 'figx';
+import { monokai } from 'react-code-blocks';
 
 type Settings = {
   collection?: string;
   mode?: string;
   useReference?: boolean;
+  format?: string;
 };
 
-type CollectionDropdownOption = DropdownOption & {value?: string}
-function Plugin({collections, settings}: {collections: VariableCollection[], settings: Settings}) {
+type CollectionDropdownOption = DropdownOption & { value?: string }
 
-  function onWindowResize(windowSize: {width: number, height: number}) {
+function Plugin({ collections, settings }: { collections: VariableCollection[], settings: Settings }) {
+
+  function onWindowResize(windowSize: { width: number, height: number }) {
     emit<ResizeWindowHandler>("RESIZE_WINDOW", windowSize);
   }
 
+  const [outputs, setOutputs] = useState<object>({});
+
+  window.addEventListener(
+    "message",
+    ({ data }) => {
+      const pluginMessage = data.pluginMessage;
+      if (pluginMessage) {
+        switch (pluginMessage.type) {
+          case "OUTPUT":
+            setOutputs(pluginMessage.data);
+            break;
+        }
+      }
+    },
+    false,
+  );
+
   useWindowResize(onWindowResize, {
-    maxHeight: 320,
-    maxWidth: 320,
-    minHeight: 120,
+    minHeight: 300,
     minWidth: 120,
     resizeBehaviorOnDoubleClick: "minimize",
   });
 
   const [collectionsList] = useState<Array<CollectionDropdownOption>>(
-      collections
-        .filter((collection) => collection.name.includes('[AWC]'))
-        .map((collection): CollectionDropdownOption => {
-          return {text: collection.name, value: collection.id}
+    collections
+      .filter((collection) => collection.name.includes('[AWC]'))
+      .map((collection): CollectionDropdownOption => {
+        return { text: collection.name, value: collection.id }
       })
   );
 
   const [modesList, setModesList] = useState<Array<DropdownOption>>([]);
+  const [showCode, setShowCode] = useState<boolean>(false);
 
-  
+  const [formatList] = useState<Array<CollectionDropdownOption>>([
+    { text: 'CSS', value: 'css' },
+    { text: 'JSON', value: 'json' },
+    { text: 'JSON Flat', value: 'json-flat' }
+  ])
+
+
   const getModeList = (collection) => {
     setModesList(collections
       .find((item) => item.id === collection)
       ?.modes
       .map((modes): DropdownOption => {
-        return {text: modes.name, value: modes.modeId}
+        return { text: modes.name, value: modes.modeId }
       }))
   }
 
@@ -57,7 +86,7 @@ function Plugin({collections, settings}: {collections: VariableCollection[], set
     },
     submit: async function (formState: Settings) {
       emit('SUBMIT', formState)
-      window.close()
+      setShowCode(true)
     },
     transform: function (formState: Settings) {
       getModeList(formState.collection)
@@ -67,21 +96,21 @@ function Plugin({collections, settings}: {collections: VariableCollection[], set
       return true;
     }
   });
-  
+
   useEffect(() => {
-    if(!formState.collection){
+    if (!formState.collection) {
       setFormState(collectionsList[0]?.value, 'collection')
       getModeList(collectionsList[0]?.value)
-    }else{
+    } else {
       getModeList(formState.collection)
     }
   }, [formState, collectionsList])
 
-  if(!collectionsList){
+  if (!collectionsList) {
     return (
       <Container space='medium'>
         <VerticalSpace space='medium' />
-  
+
         <Banner icon={<IconWarning32 />} variant="warning">
           There is no collection compatible with this plugin.
         </Banner>
@@ -96,20 +125,32 @@ function Plugin({collections, settings}: {collections: VariableCollection[], set
       <VerticalSpace space='medium' />
 
       <Text>Collection</Text>
-      <VerticalSpace space='small'  />
-      <Dropdown  onValueChange={setFormState} name="collection" options={collectionsList} value={collectionsList.find((item) => item?.value === formState.collection) ? formState.collection : collectionsList[0]?.value} variant="border" />
-      
-      { formState.collection && !!modesList.length  && (
+      <VerticalSpace space='small' />
+      <Dropdown onValueChange={setFormState} name="collection" options={collectionsList} value={collectionsList.find((item) => item?.value === formState.collection) ? formState.collection : collectionsList[0]?.value} variant="border" />
+
+      {formState.collection && !!modesList.length && (
         <Fragment>
           <VerticalSpace space='medium' />
           <Text>Variant</Text>
           <VerticalSpace space='small' />
           <Dropdown onValueChange={setFormState} name="mode" options={modesList} value={formState.mode} variant="border" />
         </Fragment>
-      ) }
+      )}
 
       <VerticalSpace space="medium" />
-      
+
+      <Fragment>
+        <Text>Format</Text>
+        <VerticalSpace space='small' />
+        <Dropdown onValueChange={setFormState} name="format"
+          options={formatList}
+          value={formatList.find((item) => item?.value === formState.format) ? formState.format : formatList[0]?.value}
+          variant="border" />
+      </Fragment>
+
+      <VerticalSpace space='medium' />
+
+
       <Toggle onValueChange={setFormState} name="useReference" value={formState.useReference}>
         <Text>Use reference</Text>
       </Toggle>
@@ -117,9 +158,40 @@ function Plugin({collections, settings}: {collections: VariableCollection[], set
       <VerticalSpace space="medium" />
 
       <Button disabled={disabled === true} fullWidth onClick={handleSubmit}>
-        Save
+        Generate output
       </Button>
-    </Container>
+
+      {outputs[formState.format] && outputs[formState.format].code && (
+        <Modal
+          title={outputs[formState.format].title}
+          open={showCode}
+          closeButtonIcon={<IconCross32 />}
+          onCloseButtonClick={() => setShowCode(false)}
+        >
+          <div style={{ padding: 0, overflow: 'auto', height: '80vh', width: '80vw', position: 'relative' }}>
+            <div style={{ position: 'absolute', right: ".5rem", top: ".5rem", zIndex: 1 }}>
+              <Button
+                onClick={() => {
+                  copyToClipboard(outputs[formState.format].code)
+                  setShowCode(false)
+                  emit('COPY_TO_CLIPBOARD', outputs[formState.format])
+                }}
+              >
+                Copy to clipboard
+              </Button>
+            </div>
+
+            <SyntaxHighlighter
+              language={outputs[formState.format].language}
+              style={themes.monokai}
+            >
+              {outputs[formState.format].code}
+            </SyntaxHighlighter>
+          </div>
+        </Modal>
+      )
+      }
+    </Container >
   )
 }
 
